dcfa5d42aa07552f0e0912d419de40aaa5258d80
HARRIISS87 <101104709+HARRIISS87@users.noreply.github.com>
2022-03-22 15:39:39 +0530

    Add files via upload

---
+++ Day12Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    class employee
+    {
+        public String name = "Haris";
+        public int salery = 1500000;
+        public int year_of_experiance = 2, age = 19, empId = 39290031;
+
+    }
+    class serialization
+    {
+        public void seritofile()
+        {
+            employee emp = new employee();
+            FileStream fs = new FileStream("Employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, emp);
+            fs.Close(); ;
+        }
+        public void serioutfile()
+        {
+            FileStream fs1 = new FileStream("Employee.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter r = new BinaryFormatter();
+            employee emp1 = (employee)r.Deserialize(fs1);
+            Console.WriteLine(emp1.name);
+            Console.WriteLine(emp1.salery);
+            Console.WriteLine(emp1.year_of_experiance);
+            
+        }
+        public static void Main()
+        {
+            serialization sere = new serialization();
+            sere.seritofile();
+            sere.serioutfile();
+        }
+    }
+}
