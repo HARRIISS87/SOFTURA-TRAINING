0bc4807f3ebc44a38c2facb1e0468fc75c5f3a61
HARRIISS87 <101104709+HARRIISS87@users.noreply.github.com>
2022-03-22 16:12:28 +0530

    Add files via upload

---
+++ Day12Task1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Task_10
+{
+    class Day12Task1
+    {
+        public static void Main()
+        {
+            Queue q = new Queue();
+            q.Enqueue("Ice cream");
+            q.Enqueue("Falooda");
+            q.Enqueue("LCappuchino");
+            q.Enqueue("Gulab jamuns");
+            FileStream fs = new FileStream("Restaurant.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter br = new BinaryWriter(fs);
+            foreach (string cal in q)
+            {
+                br.Write("\n" + cal);
+            }
+            br.Flush();
+            fs.Close();
+
+        }
+    }
+}
