601e611415890a0ede641375057853a360d3efea
HARRIISS87 <101104709+HARRIISS87@users.noreply.github.com>
2022-04-08 17:30:17 +0530

    Add files via upload

---
+++ (Haris)Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace LINQ
+{
+	class task2
+	{
+
+
+		public static void Main()
+		{
+			int Eyes = 52, Legs = 80;
+
+			int count = 0;
+			int count2 = 0;
+
+			count = 2 * (Eyes) - (Legs);
+			count = count / 2;
+
+			Console.WriteLine("No of Ducks:" + count);
+
+			count2 = 26 - count;
+			Console.WriteLine("No of Goats:" + count2);
+		}
+	}
+
+}
